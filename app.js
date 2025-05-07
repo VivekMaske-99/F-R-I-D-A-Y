2a9c32677660c4d7911f40f778d49b18ac05eb19
VivekMaske-99 <vivekmaske998@gmail.com>
2025-05-07 21:31:18 +0530

    added the whether setup and initial user data storage

--- app.js
+++ app.js
@@ -2,6 +2,51 @@
 const startbtn = document.getElementById('start')
 const stopbtn = document.getElementById('stop')
 const speakbtn = document.getElementById('speak')
+//weather setup
+function weather(location) {
+    const weatherCont = document.querySelector(".temp").querySelectorAll("*");
+  
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=48ddfe8c9cf29f95b7d0e54d6e171008`;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function () {
+      if (this.status === 200) {
+        let data = JSON.parse(this.responseText);
+        weatherCont[0].textContent = `Location : ${data.name}`;
+        weatherCont[1].textContent = `Country : ${data.sys.country}`;
+        weatherCont[2].textContent = `Weather type : ${data.weather[0].main}`;
+        weatherCont[3].textContent = `Weather description : ${data.weather[0].description}`;
+        weatherCont[4].src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        weatherCont[5].textContent = `Original Temperature : ${ktc(
+          data.main.temp
+        )}`;
+        weatherCont[6].textContent = `feels like ${ktc(data.main.feels_like)}`;
+        weatherCont[7].textContent = `Min temperature ${ktc(data.main.temp_min)}`;
+        weatherCont[8].textContent = `Max temperature ${ktc(data.main.temp_max)}`;
+        weatherStatement = `sir the weather in ${data.name} is ${
+          data.weather[0].description
+        } and the temperature feels like ${ktc(data.main.feels_like)}`;
+      } else {
+        weatherCont[0].textContent = "Weather Info Not Found";
+      }
+    };
+  
+    xhr.send();
+  }
+  
+  // convert kelvin to celcius
+  function ktc(k) {
+    k = k - 273.15;
+    return k.toFixed(2);
+  }
+//   calling weather info
+  weather("Mumbai")
+  
+  if (localStorage.getItem("jarvis_setup") !== null) {
+    weather(JSON.parse(localStorage.getItem("jarvis_setup")).location);
+  }
+  
+ 
 
 // speech recognition setup
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
