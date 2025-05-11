9a4dffc7ab8765653632fd535a5eddcebdbbbd17
VivekMaske-99 <vivekmaske998@gmail.com>
2025-05-11 20:15:15 +0530

    makes some changes

--- app.js
+++ app.js
@@ -3,9 +3,46 @@ const startbtn = document.getElementById('start')
 const stopbtn = document.getElementById('stop')
 const speakbtn = document.getElementById('speak')
 const turn_on = document.getElementById('turn_on')
-// const time = document.getElementById('times')
+const time = document.getElementById('times')
+// const msgs =  document.querySelectorAll('.message')
+// whether the recognition is stopiing on my command or automatically
+let stopingR = false;
+
+
+
+// friday command ?\
+let fridayComs = [];
+fridayComs.push("hi friday");
+fridayComs.push("what are your commands");
+fridayComs.push("close this - to close opened popups");
+fridayComs.push(
+  "change my information - information regarding your acoounts and you"
+);
+fridayComs.push("whats the weather or temperature");
+fridayComs.push("show the full weather report");
+fridayComs.push("are you there - to check fridays presence");
+fridayComs.push("shut down - stop voice recognition");
+fridayComs.push("open google");
+fridayComs.push('search for "your keywords" - to search on google ');
+fridayComs.push("open whatsapp");
+fridayComs.push("open youtube");
+fridayComs.push('play "your keywords" - to search on youtube ');
+fridayComs.push("close this youtube tab - to close opened youtube tab");
+fridayComs.push("open firebase");
+fridayComs.push("open netlify");
+fridayComs.push("open twitter");
+fridayComs.push("open my twitter profile");
+fridayComs.push("open instagram");
+fridayComs.push("open my instagram profile");
+fridayComs.push("open github");
+fridayComs.push("open my github profile");
 const jarvisbtn = document.getElementById('start_jarvis_btn').addEventListener("click",()=>{
   recognition.start()
+})
+document.querySelector("#stop_jarvis_btn").addEventListener("click", () => {
+  stopingR = true;
+  recognition.stop();
+
 })
 
 //weather setup
@@ -47,22 +84,14 @@ function weather(location) {
   }
 //   calling weather info
   weather("Mumbai")
+  // date and time 
+  // date and time
+let date = new Date();
+let hrs = date.getHours();
+let mins = date.getMinutes();
+let secs = date.getSeconds();
   
-  // let date = new Date()
-  // let hours = date.getHours()
-  // let minutes = date.getMinutes()
-  // let seconds = date.getSeconds()
-  
-  // window.onload = ()=>{
-    //   times.textContent = `${hours}:${minutes}:${seconds}`
-    //   setInterval(() =>{
-// let date = new Date()
-// let hours = date.getHours()
-// let minutes = date.getMinutes()
-// let seconds = date.getSeconds()
-//  times.textContent = `${hours}:${minutes}:${seconds}`
-//   })
-// }
+
 // autojarvis 
 function autojarvis() {
   setTimeout(() => {
@@ -70,10 +99,11 @@ function autojarvis() {
   }, 1000);
 }
 
-
 window.onload=function(){
+  turn_on.play();
 // power on audio setup 
-  turn_on.play()
+  let playedOnce = false;
+
   turn_on.addEventListener("onend",function(){
     setTimeout(() => {
       readOut("ready to go sir")
@@ -83,17 +113,36 @@ window.onload=function(){
     }, 200);
   })
 
+//friday commands
+fridayComs.forEach((e)=>{
+ document.querySelector(".command").innerHTML += `<p>#${e}</p><br/>`;
 
+})
+//   // time setup 
+// function updateClock() {
+//     const now = new Date();
+//     const timeStr = now.toLocaleTimeString();
+//     document.getElementById("times").textContent = timeStr;
+// }
 
-  // time setup 
-function updateClock() {
-    const now = new Date();
-    const timeStr = now.toLocaleTimeString();
-    document.getElementById("times").textContent = timeStr;
+// setInterval(updateClock, 1000);
+// updateClock(); // Initial call
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  currentTime = strTime
+  time.textContent = strTime
 }
 
-setInterval(updateClock, 1000);
-updateClock(); // Initial call
+formatAMPM(date)
+setInterval(() => {
+  formatAMPM(date)
+}, 60000);
 //battery setup
 navigator.getBattery().then(function(battery) {
     function updateBatteryStatus() {
@@ -120,9 +169,6 @@ updateInternetStatus();
 window.addEventListener("online", updateInternetStatus);
 window.addEventListener("offline", updateInternetStatus);
 };
-
-
-
 
   //jarvis setup
   
@@ -170,34 +216,88 @@ if (testArr.includes("")){
 }
 
 
-  
-
 // speech recognition setup
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
+recognition.continuous=true;
 //vr start
 recognition.onstart = function(){
     console.log('Speech recognition started');
 }
+
+// arr of window
+let windowsB = []
 // vr result 
 recognition.onresult=function (event){
+  // showMessage("user", transcript);
+
     let current = event.resultIndex;
     let transcript = event.results[current][0].transcript;
     transcript=transcript.toLowerCase()
     let userdata = localStorage.getItem("jarvis_setup")
+    // showMessage("usermsg",transcript)
     console.log(`my words:${transcript}`);
     if(transcript.includes("hello friday")){
         readOut("hello sir")
+        // showMessage("jarvis", "opening youtube");
     }
+    // shut down command 
+    if(transcript.includes("shut down")){
+        readOut("ok sir taking nap");
+          stopingR=true;
+        recognition.stop();
+        // showMessage("jarvis", "opening youtube");
+    }
+    if(transcript.includes("what are your commands")){
+        readOut("sir,i can perform the following tasks:")
+        document.querySelector('.command').style.display="block"
+        // showMessage("jarvis", "opening youtube");
+    }
+    if(transcript.includes("close the commands")){
+        readOut("closing commands")
+        document.querySelector('.command').style.display="none"
+        // showMessage("jarvis", "opening youtube");
+    }
+    
+
+    //showuseInfo
+      if (transcript.includes("change my information")) {
+      readOut("Opening the information tab sir");
+      localStorage.clear();
+      
+      if(window.innerWidth <= 400 ){
+        window.resizeTo(screen.width,screen.height)
+      }
+      setup.style.display = "flex";
+      setup.querySelector("button").addEventListener("click", userInfo);
+    }
+
     //for youtube cammands 
     if(transcript.includes("open youtube")){
         readOut("opening youtube")
-        window.open("https://www.youtube.com/")
+        // showMessage("jarvis", "opening youtube");
+       let a= window.open("https://www.youtube.com/")
+       windowsB.push(a)
     }
+    // some casual commands 
+     if (transcript.includes("what's the current charge?")) {
+      readOut(`the current charge is ${battery}`);
+    }
+    if (transcript.includes("what's the charging status")) {
+      readOut(`the current charging status is ${chargeStatus}`);
+    }
+    if (transcript.includes("current time")) {
+      readOut(currentTime);
+    }
+    if (transcript.includes("connection status")) {
+      readOut(`you are ${connectivity} sir`);
+    }
+
     //yt search cammand
     if(transcript.includes("play the")){
         readOut("here's the result:")
+          // showMessage("jarvis", "here's the result:");
         let input = transcript.split("")
         input.splice(0,9)
         input.pop()
@@ -211,12 +311,15 @@ recognition.onresult=function (event){
     // https://open.spotify.com/album/013jUXOfDFXnDMBetTdsiH?si=LNgrkkjwSTKBXjbAJTrRHQ
     if(transcript.includes("open spotify")){
         readOut("opening spotify")
+        //  showMessage("jarvis", "opening spotify");
         window.open("https://open.spotify.com/album/013jUXOfDFXnDMBetTdsiH?si=LNgrkkjwSTKBXjbAJTrRHQ", "_blank");
     }
 
     // to search songs on browser
     if (transcript.includes("play song")) {
         readOut("Here's the result on Spotify:");
+        // showMessage("jarvis", "Here's the result on Spotify:");
+
         let input = transcript.split("");
         input.splice(0, 10); // remove "play the"
         input.pop(); // remove final space or dot
@@ -229,6 +332,7 @@ recognition.onresult=function (event){
     //for whatsapp web
     if(transcript.includes("open whatsapp")){
         readOut("opening whatsapp")
+      //  showMessage("jarvis", "opening whatsapp");
         window.open("https://web.whatsapp.com/")
     }
 
@@ -236,11 +340,14 @@ recognition.onresult=function (event){
     //opning google
     if(transcript.includes("open google")){
         readOut("opening google")
-        window.open("https://www.google.com/")
+        //  showMessage("jarvis", "opening google");
+       let a= window.open("https://www.google.com/")
+        windowsB.push(a)
     }
     // google search cammand
     if(transcript.includes("search for")){
         readOut("here's the result:")
+        //  showMessage("jarvis", "here's the result:");
         let input = transcript.split("")
         input.splice(0,11)
         input.pop()
@@ -253,6 +360,7 @@ recognition.onresult=function (event){
     //gmail opning cammand
     if(transcript.includes("open gmail")){
      readOut("opening gmail")
+      // showMessage("jarvis", "opening gmail");
      window.open("https://mail.google.com/mail/u/0/#inbox")
 
 
@@ -265,6 +373,7 @@ recognition.onresult=function (event){
     //firebase with account feature
     if(transcript.includes("open firebase")&&transcript.includes("account")){
         readOut("opening firebase console")
+        //  showMessage("jarvis", "opening firebase console")
         let accId = transcript;
         accId= accId.split("")
         accId.pop()
@@ -277,22 +386,27 @@ recognition.onresult=function (event){
     
     if(transcript.includes("open instagram")){
       readOut("opening instagram")
+      //  showMessage("jarvis", "opening instagram");
     window.open("https://www.instagram.com/")
     }
     if(transcript.includes("open insta profile")){
       readOut("opening instagram profile")
+      //  showMessage("jarvis", "opening instagram profile")
     window.open(`https://www.instagram.com/${JSON.parse(userdata).instagram}`)
     }
     if(transcript.includes("open linkdin")){
       readOut("opening linkdin")
+      //  showMessage("jarvis", "opening linkdin");
     window.open("https://www.linkedin.com/feed/")
     }
     if(transcript.includes("open link din profile")|| transcript.includes("open link dean profile")){
       readOut("opening linkdin profile")
+      //  showMessage("jarvis", "opening linkdin profile")
     window.open(`https://www.linkedin.com/in/${JSON.parse(userdata).linkdin}`)
     }
     if(transcript.includes("open github")){
       readOut("opening github")
+      //  showMessage("jarvis", "opening github");
     window.open("https://github.com/")
     }
     if(transcript.includes("what is my name")){
@@ -300,6 +414,7 @@ recognition.onresult=function (event){
       input.splice(0,16)
       input.pop()
       readOut(`${JSON.parse(userdata).name}`)
+      //  showMessage("jarvis", `${JSON.parse(userdata).name}`);
     
     }
     if(transcript.includes("who i am")){
@@ -307,16 +422,30 @@ recognition.onresult=function (event){
       input.splice(0,9)
       input.pop()
       readOut(`${JSON.parse(userdata).bio}`)
+      //  showMessage("jarvis", `${JSON.parse(userdata).bio}`);
     
     }
+    if (transcript.includes("close all tabs")) {
+    readOut("closing all tabs sir");
+    windowsB.forEach((tabs) => {
+      tabs.close()
+    })
+}
 
 }
 
 
 //vr stop
-recognition.onend = function(){
-    console.log('Speech recognition stoped');
-}
+recognition.onend = function () {
+  if (stopingR === false) {
+    setTimeout(() => {
+      recognition.start();
+    }, 500);
+  } else if (stopingR === true) {
+    recognition.stop();
+    document.querySelector("#stop_jarvis_btn").style.display = "none"
+  }
+};
 //vr continous
 recognition.continuous= true;
 
@@ -328,6 +457,7 @@ stopbtn.addEventListener("click",()=>{
     recognition.stop();
 })
 
+
 // friday speech
 function readOut(message){
    const speech = new SpeechSynthesisUtterance(message);
@@ -337,10 +467,5 @@ function readOut(message){
    speech.volume= 1;
    window.speechSynthesis.speak(speech)
    console.log("speaking out");
+  //  showMessage("friday",message)
 }
-speakbtn.addEventListener("click",()=>{
-    readOut("hii vivek i love you")
-})
-// window.onload = function(){
-//  readOut("    ");
-// }
